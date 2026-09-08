8dce7083b33fd7ab1be11a470f5ecc88e509f0c9
LiWenshan <amicorati@gmail.com>
2026-09-08 14:31:46 +0800

    feat: use shared alarm shortcut with callback

--- test/sw.js
+++ test/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-timetable-shell-v1.0.30';
+const CACHE_NAME = 'static-timetable-shell-v1.0.32';
 const APP_SHELL = [
   './', './index.html', './styles.css', './app.js', './manifest.json',
   './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'
